21c14e151376b3a24e365e3bb90b9f7adbf6ea48
kinjo yuji <kinjoyuji@gmail.com>
2018-03-13 22:01:08 +0900

    FORMのUI実装

---
+++ src/containers/form.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const Form = ({ handleOnChange, handleOnSubmit }) => {
+  return(
+    <form onSubmit={(e) => handleOnSubmit()}>
+      <fieldset>
+        <input
+          type="text"
+          onChange={(e) => handleOnChange(e.target.value)}
+        />
+        <input type="submit"/>
+      </fieldset>
+    </form>
+  );
+}
+
+export default Form;
---
+++ src/containers/result.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Result = ({ text }) => {
+  return (
+    <p>
+      {text}
+    </p>
+  );
+}
+
+export default Result;
